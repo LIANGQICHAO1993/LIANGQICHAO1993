cd989e2e7dafe0ac0d239eae3e1b6c9450b4d2e0
LIANGQICHAO1993 <qichaoliang1984@163.com>
2021-07-07 11:03:46 +0800

    first commit

---
+++ blogproject_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ckl\desktop\my_project\liangqichao1993\blogproject_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
